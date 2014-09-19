21e8bbb9e381e2b21e20fef03c24242e08cf43f0
Mbechezi Mlanawo <mlanawo.mbechezi@ikimea.com>
2014-09-20 00:08:49 +0200

    syncro with https://github.com/tempo-project/Resource

---
+++ Twig/ResourceExtension.php
@@ -0,0 +1,72 @@
+<?php
+
+/*
+* This file is part of the Tempo-project package http://tempo-project.org/>.
+*
+* (c) Mlanawo Mbechezi  <mlanawo.mbechezi@ikimea.com>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+namespace Tempo\Component\Resource\Twig;
+
+use Ikimea\Browser\Browser;
+
+
+class ResourceExtension extends \Twig_Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getFunctions()
+    {
+        return array(
+            'get_browser' => new \Twig_Function_Method($this, 'getBrowser'),
+            'gravatar'    => new \Twig_Function_Method($this, 'getGravatar'),
+        );
+    }
+
+    /**
+     * @return string
+     */
+    public function getBrowser()
+    {
+        $browser = new Browser() ;
+        $navigateurFinal = explode('.', $browser->getVersion() );
+
+        return strtolower($browser->getBrowser(). ' ' .
+            $browser->getBrowser().$navigateurFinal[0]). ' '.
+        $browser->getPlatform();
+    }
+
+    // get gravatar image
+    public function getGravatar($email, $size = null, $default = null, $rating = null, $secure = null)
+    {
+        $defaults = array(
+            'size'    => 80,
+            'rating'  => 'g',
+            'default' => null,
+            'secure'  => false,
+        );
+
+        $map = array(
+            's' => $size    ?: $defaults['size'],
+            'r' => $rating  ?: $defaults['rating'],
+            'd' => $default ?: $defaults['default'],
+        );
+
+        $hash = md5(strtolower(trim($email)));
+
+        if (null === $secure) {
+            $secure = $defaults['secure'];
+        }
+
+        return ($secure ? 'https://secure' : 'http://www') . '.gravatar.com/avatar/' . $hash . '?' . http_build_query(array_filter($map));
+    }
+
+    public function getName()
+    {
+       return 'resource';
+    }
+}
